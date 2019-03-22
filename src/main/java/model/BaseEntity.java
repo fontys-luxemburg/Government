a533370e95d8372aa4d3ece2a098d726fa201b8a
tjidde-nl <tjidde-nl@users.noreply.github.com>
2019-03-22 10:40:29 +0100

    Create BaseEntity.java

---
+++ src/main/java/model/BaseEntity.java
@@ -0,0 +1,56 @@
+package model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.util.Date;
+
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+	@Id @GeneratedValue
+	private Long id;
+
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private Date createdAt;
+
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private Date updatedAt;
+
+	public BaseEntity() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdateAt() {
+		return updatedAt;
+	}
+
+	public void setUpdateAt(Date updateAt) {
+		this.updatedAt = updateAt;
+	}
+
+	public boolean isNew() {
+		return createdAt == null;
+	}
+}
