fb670d0bc132f29309aad0773db43d6e16fc9421
Tom Bakker <tom.bakker@truetrack.nl>
2019-04-05 12:29:37 +0200

    Dit zou het moeten zijn

---
+++ src/main/java/model/Vehicle.java
@@ -0,0 +1,53 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "vehicles")
+public class Vehicle extends BaseEntity {
+
+    @NotNull
+    @NotBlank
+    private String registrationID;
+
+    private String brand;
+
+    private String model;
+
+    private String bodyType;
+
+    public String getRegistrationID() {
+        return registrationID;
+    }
+
+    public void setRegistrationID(String registrationID) {
+        this.registrationID = registrationID;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public void setBodyType(String bodyType) {
+        this.bodyType = bodyType;
+    }
+}
---
+++ src/main/java/repository/VehicleRepository.java
@@ -0,0 +1,29 @@
+package repository;
+
+import model.Vehicle;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import java.util.Optional;
+
+@ApplicationScoped
+public class VehicleRepository extends CrudRepository<Vehicle, Long> {
+
+    @PostConstruct
+    private void init() {
+        setEntityClass(Vehicle.class);
+    }
+
+    public Optional<Vehicle> findByRegistrationID(String registrationID) {
+        Query query = entityManager.createQuery("select v from Vehicle v where v.registrationID = :registrationID");
+        query.setParameter("registrationID", registrationID);
+
+        try {
+            return Optional.of((Vehicle) query.getSingleResult());
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+}
