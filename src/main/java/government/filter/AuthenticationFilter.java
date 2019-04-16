6535157a76c17c43e0acff76c9a0a71f2e19cb24
OlafvanDijk <o.olaf@hotmail.com>
2019-04-16 12:56:02 +0200

    jwt and get all vehicle

---
+++ src/main/java/government/filter/AuthenticationFilter.java
@@ -0,0 +1,122 @@
+package government.filter;
+
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import government.annotation.Secured;
+import government.model.Role;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Secured
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class AuthenticationFilter implements ContainerRequestFilter {
+
+    private static final String REALM = "example";
+    private static final String AUTHENTICATION_SCHEME = "Bearer";
+    private String ISSUER = "government_api";
+
+    @Context
+    private ResourceInfo resourceInfo;
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
+        // Get the Authorization header from the request
+        String authorizationHeader =
+                requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+
+        // Validate the Authorization header
+        if (!isTokenBasedAuthentication(authorizationHeader)) {
+            abortWithUnauthorized(requestContext);
+            return;
+        }
+
+        // Extract the token from the Authorization header
+        String token = authorizationHeader
+                .substring(AUTHENTICATION_SCHEME.length()).trim();
+
+        try {
+
+            // Validate the token
+            validateToken(token);
+
+        } catch (Exception e) {
+            abortWithUnauthorized(requestContext);
+        }
+    }
+
+    private boolean isTokenBasedAuthentication(String authorizationHeader) {
+
+        // Check if the Authorization header is valid
+        // It must not be null and must be prefixed with "Bearer" plus a whitespace
+        // The authentication scheme comparison must be case-insensitive
+        return authorizationHeader != null && authorizationHeader.toLowerCase()
+                .startsWith(AUTHENTICATION_SCHEME.toLowerCase() + " ");
+    }
+
+    private void abortWithUnauthorized(ContainerRequestContext requestContext) {
+
+        // Abort the filter chain with a 401 status code response
+        // The WWW-Authenticate header is sent along with the response
+        requestContext.abortWith(
+                Response.status(Response.Status.UNAUTHORIZED)
+                        .header(HttpHeaders.WWW_AUTHENTICATE,
+                                AUTHENTICATION_SCHEME + " realm=\"" + REALM + "\"")
+                        .build());
+    }
+
+    private void validateToken(String token) throws Exception {
+        // Check if the token was issued by the server and if it's not expired
+        // Throw an Exception if the token is invalid
+        Algorithm algorithm = Algorithm.HMAC256("secret_key");
+        JWTVerifier verifier = com.auth0.jwt.JWT.require(algorithm)
+                .withIssuer(ISSUER)
+                .build(); //Reusable verifier instance
+
+        DecodedJWT jwt = verifier.verify(token);
+
+        Method method = resourceInfo.getResourceMethod();
+
+        if (method != null) {
+            String roles = jwt.getClaim("Role").asString();
+            Role roleUser = Role.valueOf(roles);
+
+            List<Role> methodRoles = extractRoles(method);
+
+            if (!methodRoles.contains(roleUser)) {
+                throw new Exception("no valid Role.");
+            }
+        }
+    }
+
+    // Extract the roles from the annotated element
+    private List<Role> extractRoles(AnnotatedElement annotatedElement) {
+        if (annotatedElement == null) {
+            return new ArrayList<Role>();
+        } else {
+            Secured secured = annotatedElement.getAnnotation(Secured.class);
+            if (secured == null) {
+                return new ArrayList<Role>();
+            } else {
+                Role[] allowedRoles = secured.value();
+                return Arrays.asList(allowedRoles);
+            }
+        }
+    }
+}
