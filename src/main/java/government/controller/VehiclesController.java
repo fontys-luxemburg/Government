f3f951b032c1df87f361561aa6e6aa77bfb9fbf9
arjanl94 <arjanlombarts@gmail.com>
2019-05-10 12:23:05 +0200

    refactor

--- src/main/java/government/controller/VehiclesController.java
+++ src/main/java/government/controller/VehiclesController.java
@@ -1,9 +1,6 @@
 package government.controller;
 
-import com.sun.deploy.net.HttpResponse;
-import government.annotation.Secured;
 import government.dto.TrackerIdDto;
-import government.dto.UserDto;
 import government.dto.VehicleDto;
 import government.facade.TrackerIdFacade;
 import government.dto.VehicleInformationDto;
@@ -12,42 +9,34 @@ import government.model.TrackerId;
 import government.facade.OwnershipFacade;
 import government.facade.UserFacade;
 import government.facade.VehicleFacade;
-import government.model.TrackerId;
 import government.mapper.TrackerIdMapper;
 import government.mapper.VehicleMapper;
-import government.model.Role;
 import government.model.User;
 import government.model.Vehicle;
 import government.model.VehicleInformation;
-import jdk.nashorn.internal.objects.annotations.Getter;
-import javax.swing.text.html.Option;
 
 import javax.inject.Inject;
-import javax.net.ssl.HttpsURLConnection;
 import javax.json.JsonObject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Path("/vehicles")
 @Produces("application/json")
-@Secured({Role.Employee, Role.Admin})
 public class VehiclesController {
 
     @Inject
-    VehicleFacade facade;
+    VehicleFacade vehicleFacade;
 
     @Inject
     TrackerIdFacade trackerIdFacade;
@@ -71,7 +60,7 @@ public class VehiclesController {
     @Path("{registration_id}")
     @Transactional
     public Response show(@PathParam("registration_id") String registrationID) {
-        Optional<Vehicle> vehicle = facade.findByRegistrationID(registrationID);
+        Optional<Vehicle> vehicle = vehicleFacade.findByRegistrationID(registrationID);
 
         if (!vehicle.isPresent()) {
             return Response.status(Response.Status.NOT_FOUND).build();
@@ -83,7 +72,7 @@ public class VehiclesController {
     @GET
     @Path("{registration_id}/ownerships")
     public Response showOwnerships(@PathParam("registration_id") String registrationID) {
-        Optional<Vehicle> vehicle = facade.findByRegistrationID(registrationID);
+        Optional<Vehicle> vehicle = vehicleFacade.findByRegistrationID(registrationID);
 
         if(!vehicle.isPresent()) {
             return Response.status(Response.Status.NOT_FOUND).build();
@@ -96,7 +85,7 @@ public class VehiclesController {
     @Path("{registration_id}/ownerships")
     @Transactional
     public Response transfersOwnership(@PathParam("registration_id") String registrationID, JsonObject params) {
-        Optional<Vehicle> vehicle = facade.findByRegistrationID(registrationID);
+        Optional<Vehicle> vehicle = vehicleFacade.findByRegistrationID(registrationID);
 
         if(!vehicle.isPresent()) {
             return Response.status(Response.Status.NOT_FOUND).build();
@@ -114,7 +103,7 @@ public class VehiclesController {
 
         Vehicle vehicle = vehicleMapper.vehicleDtoToVehicle(vehicleDto);
 
-        vehicle = facade.save(vehicle);
+        vehicle = vehicleFacade.save(vehicle);
 
         if (vehicle.getId() == null) {
             return Response.status(Response.Status.BAD_REQUEST).build();
@@ -142,7 +131,7 @@ public class VehiclesController {
 
         TrackerId trackerId = new TrackerId();
         trackerId.setTrackerId(uuid);
-        Vehicle vehicle = facade.findById(vehicleId).get();
+        Vehicle vehicle = vehicleFacade.findById(vehicleId).get();
         trackerId.setVehicle(vehicle);
 
         trackerIdFacade.save(trackerId);
@@ -154,7 +143,7 @@ public class VehiclesController {
     @Path("/{id}/trackers")
     @Transactional
     public Response getAllTrackers(@PathParam("id") long id) {
-        Optional<Vehicle> vehicle = facade.findById(id);
+        Optional<Vehicle> vehicle = vehicleFacade.findById(id);
         if (!vehicle.isPresent()) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
@@ -166,7 +155,7 @@ public class VehiclesController {
     @Path("/{id}/information")
     @Transactional
     public Response getVehicleInformation(@PathParam("id") long id) {
-        Optional<Vehicle> vehicle = facade.findById(id);
+        Optional<Vehicle> vehicle = vehicleFacade.findById(id);
         if (!vehicle.isPresent()) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
@@ -178,13 +167,13 @@ public class VehiclesController {
     @Path("/{id}/information")
     @Transactional
     public Response updateVehicleInformation(@PathParam("id") long id, VehicleInformationDto vehicleInformationDto) {
-        Optional<Vehicle> vehicle = facade.findById(id);
+        Optional<Vehicle> vehicle = vehicleFacade.findById(id);
         if (!vehicle.isPresent()) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
         VehicleInformation vehicleInformation = vehicleInformationMapper.vehicleInformationDtoToVehicleInformation(vehicleInformationDto);
         vehicle.get().setVehicleInformation(vehicleInformation);
-        facade.save(vehicle.get());
+        vehicleFacade.save(vehicle.get());
         return Response.ok().build();
     }
 
