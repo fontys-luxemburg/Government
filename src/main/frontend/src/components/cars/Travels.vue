7f80f17b24c8530e310d953065f26d6d0517fd14
OlafvanDijk <o.olaf@hotmail.com>
2019-06-04 14:03:17 +0200

    Auto stash before merge of "issue/27" and "origin/issue/27"

--- src/main/frontend/src/components/cars/Travels.vue
+++ src/main/frontend/src/components/cars/Travels.vue
@@ -1,13 +1,43 @@
 <template>
-  <car-header />
+  <div>
+    <car-header />
+    <div class="bg-white rounded p-8">
+      <la-cartesian autoresize :bound="[0]" :data="values">
+        <la-line prop="km"></la-line>
+        <la-y-axis></la-y-axis>
+        <la-x-axis prop="name"></la-x-axis>
+        <la-tooltip></la-tooltip>
+      </la-cartesian>
+    </div>
+  </div>
 </template>
 
 <script>
     import CarHeader from "./CarHeader";
+    import { Cartesian, Line } from 'laue'
+
     export default {
-        name: "Travels",
-        components: {CarHeader}
-    }
+      name: "Travels",
+      components: {
+        LaCartesian: Cartesian,
+        LaLine: Line,
+        CarHeader
+      },
+
+      data: () => ({
+        values: [
+          { name: 'Page A', km: 50 },
+          { name: 'Page B', km: 23 },
+          { name: 'Page C', km: 280 },
+          { name: 'Page D', km: 4 },
+          { name: 'Page E', km: 1 },
+          { name: 'Page F', km: 94 },
+          { name: 'Page G', km: 230 },
+          { name: 'Page H', km: 13 },
+          { name: 'Page I', km: 14 }
+        ]
+      })
+}
 </script>
 
 <style scoped>
